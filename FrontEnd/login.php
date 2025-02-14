1fa1ad62c84cecf51b2ae6264d07e45c625013cb
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-02-14 03:54:15 +0000

    Update login.php

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -6,7 +6,7 @@
 
   <body>
     <h2>Login</h2>
-    <form action="authenticate.php" method="POST"> //Form action is where we can do authentication.
+    <form action="authenticate.php" method="POST"> <!-- Form action is where we can do authentication. -->
       
       <label>Username:</label>
       <input type="text" id=username" name="username" required> <br>
@@ -14,7 +14,7 @@
       <label>Password:</label>
       <input type="text" id=password" name="password" required> <br>
 
-      <a href="register.php">New User? Register Here.</a>
+      <h3><a href="register.php">New User? Register Here.</a></h3> <br>
 
       <input type="submit" value="Log In">
     </form>
@@ -22,6 +22,9 @@
   <?php 
     //This is where we will put our POST stuff.
   ?>
+    
+  </body>
+</html>
     
   </body>
 </html>
