63ba9680c2e314ceedc198472a4fbe0283ce0494
Cristian <cristianmsinchi@gmail.com>
2025-02-15 19:43:25 -0500

    feat: Created Database Folder. Added Bash Script to create database and tables. Added Modified sqlconnect php file from tutorial

---
+++ Database/ccagDBsetup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+# Run this script to set up the database and tables in MySQLServer. Requires sudo access 
+
+db_name="ccagDB"
+db_user="ccagUser"
+db_pass="12345"  #TODO store password elsewhere (config file?)
+
+#TODO add session table
+
+sql_query=$(cat <<EOF
+
+    CREATE DATABASE IF NOT EXISTS \`${db_name}\`;
+    CREATE USER IF NOT EXISTS '${db_user}'@'localhost' IDENTIFIED BY '${db_pass}';
+    GRANT ALL ON \`${db_name}\`.* TO '${db_user}'@'localhost';
+    FLUSH PRIVILEGES;
+
+    USE \`${db_name}\`;
+
+    CREATE TABLE IF NOT EXISTS accounts (
+        uid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(32) UNIQUE NOT NULL,
+        email VARCHAR(320) UNIQUE NOT NULL,
+        password VARCHAR(150) NOT NULL
+    );
+EOF
+)
+
+sudo mysql -e "$sql_query"
+
+echo "Databased created!"
---
+++ Database/mysqlconnect.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+
+//use 127.0.0.1 to connect to your local mysql server, otherwise use designated database server ip address
+$mydb = new mysqli('127.0.0.1','ccagUser','12345','ccagDB');
+
+if ($mydb->errno != 0)
+{
+	echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+	exit(0);
+}
+
+echo "successfully connected to database".PHP_EOL;
+
+$query = "select * from accounts;";
+
+$response = $mydb->query($query);
+if ($mydb->errno != 0)
+{
+	echo "failed to execute query:".PHP_EOL;
+	echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+	exit(0);
+}
+//if successful, echos out all usernames from accounts table
+else { 
+	while ($r = mysqli_fetch_assoc($response)) {
+		echo $r['username'].PHP_EOL;
+	}
+}
+
+
+?>
