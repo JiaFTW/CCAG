d13c1ad9a74bdc5a04ed8bb97a2d86ebd04f746e
Gary <shigary99@gmail.com>
2025-02-16 12:53:13 -0500

    Update register.php

--- FrontEnd/register.php
+++ FrontEnd/register.php
@@ -1,3 +1,29 @@
 <?php 
+require_once('getDatabase.php');
+$db = getDB();
+
+$username = filter_input(INPUT_POST, 'username');
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
+$hash = password_hash($password, PASSWORD_DEFAULT); //hashes the password
+
+$query = 'INSERT INTO accounts
+(username,email,password)
+VALUES
+(:username, :email, :password)';
+
+$statement = $db->prepare($query);
+$statement->bindValue(':username', $email);
+$statement->bindValue(':email', $email);
+$statement->bindValue(':password', $hash);
+
+if ($statement->execute()) {
+    echo "User Successfully Register!";
+}
+else {
+    echo "Could not register User" . $statement->error;
+}
+
+$statement->close();
 
 ?>
