b243ee40b81fd85b1bed4b51e06467b32a4de0e1
Cristian <cristianmsinchi@gmail.com>
2025-02-19 18:42:36 -0500

    feat: added query handler function fix: adjusted addAccountto use handler

--- Database/dbRegisterLib.php
+++ Database/dbRegisterLib.php
@@ -1,19 +1,28 @@
 <?php
 
-//WIP add user function 
-function addUser($username, $email ,$password, mysqli $db) {
-    $query = "insert into accounts 
-    ('username', 'email', 'password') 
-    values ('.$username.', '.$email.', '.$password.')";
+//WIP handler for SQL queries
+function handleQuery($q, mysqli $db, $msg = 'Query Successful') {
 
-    $response = $db->query($query);
+    $response = $db->query($q);
     if ($db->errno != 0) {
         echo "failed to execute query:".PHP_EOL;
         echo __FILE__.':'.__LINE__.":error: ".$db->error.PHP_EOL;
     }
     else {
-        echo "Successfully added user";
+        echo $msg.PHP_EOL;
+        return $response;
     }
+}
+
+
+//WIP add user function 
+function addAccount($username, $email ,$password, mysqli $db) {
+    $query = "insert into accounts 
+    ('username', 'email', 'password') 
+    values ('.$username.', '.$email.', '.$password.')";
+
+    handleQuery($query, $db, "Added Account to Database"); 
+    
 }
 
 
