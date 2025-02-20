c537b32b79507386de0fbfae9f1684b9a2305793
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-02-20 15:08:36 -0500

    Update login.php

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -1,33 +1,9 @@
 <?php 
-require_once('getDatabase.php');
+require_once('testRabbitMQClient.php');
 
-//Initializes $_SESSION so we can store variables to be used across multiple pages, will mainly used for keeping the user logged in when switching pages when we get basic authentication up and working.
-//session_start();
-
-$db = getDB();
 
 $username = filter_input(INPUT_POST, 'username');
 $password = filter_input(INPUT_POST, 'password');
 
-$query = 'SELECT password FROM accounts WHERE username = :username';
-
-$statement = $db->prepare($query);
-$statement->bindValue(':username', $username);
-$statement->execute();
-$row = $statement->fetch();
-$statement->close();  
-
-if ($row === false) {
-  return false;
-}
-else {
-  $hash = $row['password']; 
-  if (password_verify($password, $hash)) { // https://www.php.net/manual/en/function.password-verify.php
-    echo 'Password is Valid';
-  }
-  else {
-    echo 'Invalid credentials.';
-  }
-}
 
 ?>
