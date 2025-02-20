7fa15b2663f0e51fbede79641ef1877ddb2082f3
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-02-20 15:10:37 -0500

    Update register.php

--- FrontEnd/register.php
+++ FrontEnd/register.php
@@ -1,29 +1,10 @@
 <?php 
-require_once('getDatabase.php');
-$db = getDB();
+require_once('testRabbitMQClient.php');
 
-$username = filter_input(INPUT_POST, 'username');
-$email = filter_input(INPUT_POST, 'email');
-$password = filter_input(INPUT_POST, 'password');
-$hash = password_hash($password, PASSWORD_DEFAULT); //hashes the password
-
-$query = 'INSERT INTO accounts
-(username,email,password)
-VALUES
-(:username, :email, :password)';
-
-$statement = $db->prepare($query);
-$statement->bindValue(':username', $email);
-$statement->bindValue(':email', $email);
-$statement->bindValue(':password', $hash);
-
-if ($statement->execute()) {
-    echo "User Successfully Registered!";
-}
-else {
-    echo "Could not register user" . $statement->error;
-}
-
-$statement->close();
+$registerdata = array(
+    'username' => filter_input(INPUT_POST,'username'),
+    'email' => filter_input(INPUT_POST,'email'),
+    'password' => filter_input(INPUT_POST,'password')
+);
 
 ?>
