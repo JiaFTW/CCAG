8850fad215d0fb7e849e4c11150344b7ed98ff75
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-02-23 08:44:28 +0000

    Update login.php

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -1,9 +1,17 @@
 <?php 
-require_once('testRabbitMQClient.php');
+require_once('../rabbitmq/testRabbitMQClient.php');
 
+$logindata = array (
+    'type' => 'login',
+    'username' => filter_input(INPUT_POST,'username'),
+    'email' => filter_input(INPUT_POST,'email'),
+    'password' => filter_input(INPUT_POST,'password'),
+    'message' => 'Logging in user',
+);
 
-$username = filter_input(INPUT_POST, 'username');
-$password = filter_input(INPUT_POST, 'password');
-
+echo($logindata['type']);
+echo($logindata['username']);
+sendMessage($logindata);
 
 ?>
+
