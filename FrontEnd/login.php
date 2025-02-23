04112982e84056288785b8569d5730c11be41968
Gary <shigary99@gmail.com>
2025-02-23 11:51:58 -0500

    Update login.php

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -1,6 +1,8 @@
 <?php 
 require_once('../rabbitmq/testRabbitMQClient.php');
 
+
+
 $logindata = array (
     'type' => 'login',
     'username' => filter_input(INPUT_POST,'username'),
@@ -13,16 +15,16 @@ echo($logindata['type']);
 echo($logindata['username']);
 sendMessage($logindata);
 
-/*if doLogin($logindata['username'], $logindata['password']) {
+/* if doLogin($logindata['username'], $logindata['password']) {
 echo ("User successfully logged in!");
 session_start();
-$_SESSION['username'] = $logindata['username'];
-//setcookie("username", $logindata['username']);
+$_SESSION['user'] = $logindata['username'];
+setcookie("user", $logindata['username'], time() + (7*24*60*60), "/");
 }
 else {
 echo ("Login was not successful");
 }
-
 */
+
 ?>
 
