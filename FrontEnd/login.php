96428d26276702595c2acb7064c06efc94333f50
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-02-23 12:08:20 -0500

    Update login.php

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -15,16 +15,8 @@ echo($logindata['type']);
 echo($logindata['username']);
 sendMessage($logindata);
 
-/* if doLogin($logindata['username'], $logindata['password']) {
-echo ("User successfully logged in!");
-session_start();
-$_SESSION['user'] = $logindata['username'];
-setcookie("user", $logindata['username'], time() + (7*24*60*60), "/");
-}
-else {
-echo ("Login was not successful");
-}
-*/
+//header("Location: homepage.php");
+//exit();
 
 ?>
 
