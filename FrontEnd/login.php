acadb4bea9dc8cc2b994568e5708e34f2ee04df4
Al Kazi <ama275@njit.edu>
2025-02-24 04:16:13 -0500

    added 3 strictly necessary types of cookies the authintication cookie,session Id, CSRF,and remember_me token, for me when i tested it everything was fine but we need to check it when we connect with the database, alsothe session ID and the authentication token will store information and get information from the database, i wasn't able to do anything about that since i didn't touch the databse so we need to work on it, i commented out somestuff that i wrote that we may or may not add depending on what you guys think.

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -1,6 +1,16 @@
 <?php 
 require_once('../rabbitmq/testRabbitMQClient.php');
 
+// Validate CSRF token
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!isset($_POST['csrf_token']) || !isset($_COOKIE['csrf_token'])) {
+        die("CSRF token missing.");
+    }
+    if ($_POST['csrf_token'] !== $_COOKIE['csrf_token']) {
+        die("CSRF token validation failed.");
+    }
+}
+
 
 
 $logindata = array (
@@ -14,6 +24,29 @@ $logindata = array (
 echo($logindata['type']);
 echo($logindata['username']);
 sendMessage($logindata);
+
+// Generate a secure auth token
+$auth_token = bin2hex(random_bytes(32));
+
+/* next we can store the auth token in the database (or session) associated with the user, but for that we need sinchi's help i am king of clueless here.
+Example: saveAuthTokenToDatabase($logindata['username'], $auth_token);
+ */
+
+
+// then set the auth_token cookie
+setcookie('auth_token', $auth_token, time() + 3600, '/'); // this will expires in 1 hour
+
+// Generate a secure remember_me token
+$remember_me_token = bin2hex(random_bytes(32));
+
+/* again we have to store the remember_me token in the database (or session) associated with the user
+ Example: saveRememberMeTokenToDatabase($logindata['username'], $remember_me_token);
+ */
+
+// then set the remember_me cookie
+setcookie('remember_me', $remember_me_token, time() + (86400 * 30), '/'); // Expires in 30 days
+
+// Redirect to homepage after successful login
 
 //header("Location: homepage.php");
 //exit();
