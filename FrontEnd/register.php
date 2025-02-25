efada2ce42048bcfcba7c936aff8e73b747ae0e4
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-02-25 14:10:08 -0500

    Update register.php

--- FrontEnd/register.php
+++ FrontEnd/register.php
@@ -1,1 +1,15 @@
+<?php 
+require_once('../rabbitmq/testRabbitMQClient.php');
 
+$registerdata = array(
+    'type' => 'register',
+    'username' => filter_input(INPUT_POST,'username'),
+    'email' => filter_input(INPUT_POST,'email'),
+    'password' => filter_input(INPUT_POST,'password'),
+    'message' => 'Registering user',
+);
+
+echo ($registerdata['username']);
+sendMessage($registerdata);
+
+?>
