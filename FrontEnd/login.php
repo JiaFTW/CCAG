2c327d2bf6176369be9284f041be788ca451b393
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-02-25 04:37:54 -0500

    Update login.php

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -11,10 +11,16 @@ $logindata = array (
     'message' => 'Logging in user',
 );
 
-echo($logindata['type']);
-echo($logindata['username']);
-sendMessage($logindata);
+$response = sendMessage($logindata);
 
+if ($response['success']) {
+    header('Location: loginPage.html?message=Logged in.');
+    exit();
+} 
+else {
+    header('Location: loginPage.html?message=Invalid Username/Password.');
+    exit();
+}
 //header("Location: homepage.php");
 //exit();
 
