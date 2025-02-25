7988860c6fcffc6fff86ec68d923fa5214a08d14
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-02-25 04:37:54 -0500

    Update login.php

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -48,6 +48,14 @@ setcookie('remember_me', $remember_me_token, time() + (86400 * 30), '/'); // Exp
 
 // Redirect to homepage after successful login
 
+if ($response['success']) {
+    header('Location: loginPage.html?message=Logged in.');
+    exit();
+} 
+else {
+    header('Location: loginPage.html?message=Invalid Username/Password.');
+    exit();
+}
 //header("Location: homepage.php");
 //exit();
 
