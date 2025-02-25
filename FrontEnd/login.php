914bd80493df7f4ea91378cf99a02848752d5028
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-02-25 14:09:07 -0500

    Update login.php

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -1,1 +1,27 @@
+<?php 
+require_once('../rabbitmq/testRabbitMQClient.php');
 
+
+
+$logindata = array (
+    'type' => 'login',
+    'username' => filter_input(INPUT_POST,'username'),
+    'email' => filter_input(INPUT_POST,'email'),
+    'password' => filter_input(INPUT_POST,'password'),
+    'message' => 'Logging in user',
+);
+
+$response = sendMessage($logindata);
+
+if ($response['success']) {
+    header('Location: loginPage.html?message=Logged in.');
+    exit();
+} 
+else {
+    header('Location: loginPage.html?message=Invalid Username/Password.');
+    exit();
+}
+//header("Location: homepage.php");
+//exit();
+
+?>
