5631de989a69abb1ce4608975b7870209f74d690
Gary <shigary99@gmail.com>
2025-02-25 22:45:52 -0500

    Update login.php

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -14,6 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 
+
 //TODO : make new sendMessage type for Session Validation / Check
 $logindata = array (
     'type' => 'login',
@@ -23,35 +24,29 @@ $logindata = array (
     'message' => 'Logging in user',
 );
 
-echo($logindata['type']);
-echo($logindata['username']);
-sendMessage($logindata);
+//Sends the login request
+$response = sendMessage($logindata);
 
-// Generate a secure auth token
-$auth_token = bin2hex(random_bytes(32));
+if ($response['status'] == 'Success') {
+    // Generate a secure auth token
+    $auth_token = bin2hex(random_bytes(32));
 
-/* next we can store the auth token in the database (or session) associated with the user, but for that we need sinchi's help i am king of clueless here.
-Example: saveAuthTokenToDatabase($logindata['username'], $auth_token);
- */
+    //next we can store the auth token in the database (or session) associated with the user, but for that we need sinchi's help i am king of clueless here.
+    saveAuthTokenToDatabase($logindata['username'], $auth_token);
+    // then set the auth_token cookie
+    setcookie('auth_token', $auth_token, time() + 3600, '/'); // this will expires in 1 hour
 
+    // Generate a secure remember_me token
+    $remember_me_token = bin2hex(random_bytes(32));
+    saveRememberMeTokenToDatabase($logindata['username'], $remember_me_token);
+    setcookie('remember_me', $remember_me_token, time() + (86400 * 30), '/'); // Expires in 30 days
 
-// then set the auth_token cookie
-setcookie('auth_token', $auth_token, time() + 3600, '/'); // this will expires in 1 hour
+    header("Location: homepage.php");
+    exit();
+} else {
+    echo "Login failed: " . $response['message'];
+}
 
-// Generate a secure remember_me token
-$remember_me_token = bin2hex(random_bytes(32));
-
-/* again we have to store the remember_me token in the database (or session) associated with the user
- Example: saveRememberMeTokenToDatabase($logindata['username'], $remember_me_token);
- */
-
-// then set the remember_me cookie
-setcookie('remember_me', $remember_me_token, time() + (86400 * 30), '/'); // Expires in 30 days
-
-// Redirect to homepage after successful login
-
-//header("Location: homepage.php");
-//exit();
 
 ?>
 
