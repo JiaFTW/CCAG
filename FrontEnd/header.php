e55e8e1bcb83aff55fbaeb3436f617485ffc175c
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-02-26 23:20:20 -0500

    Update header.php

--- FrontEnd/header.php
+++ FrontEnd/header.php
@@ -10,12 +10,14 @@
                 if (isset($_SESSION['is_valid_user'])) { ?>
 
                     <a href="secretpage.php">Secret Page</a> 
-
-                    <?php 
-                    echo "Logged in: ", $_SESSION['user'];
-                    
-                }
-            } 
+                    <a><?php echo "Logged in: ", $_SESSION['user'];?></a>
+    
+               <?php }
+                else { ?>
+                      
+                    <a href="loginPage.php">Login</a>
         
+                <?php}
+            }?>
     </div>
 </header>
