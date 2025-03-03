9d0ab55481b0cbc2c80c3e8980da987c627c8359
JiaFTW <shigary99@gmail.com>
2025-03-03 06:55:28 +0000

    Added some frontpages

--- FrontEnd/profilePage.php
+++ FrontEnd/profilePage.php
@@ -1,17 +1,17 @@
 <?php
-require_once('sessionValidate.php');
+require_once('sessionValidate.php')
 ?>
 
 
 <html>
     <head>
-    <title>CCAG Page 1</title>
+    <title>CCAG Profile</title>
     <link rel="stylesheet" href="./styles/styles.css">
   </head>
     <body>
     <?php include('header.php'); ?>
         <main>
-            <p>This is page 1</p>
+            <p>This is page 2</p>
         </main>
     </body>
 </html>
