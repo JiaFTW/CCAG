50771995cd3e212e30f9e9f9eed20fa00c59ddf7
Chris <cn7@njit.edu>
2025-03-03 14:31:07 +0000

    Made server file and edited ini file to include DMZServer information

---
+++ rabbitmq/DMZServer.php
@@ -0,0 +1,69 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('../Database/mysqlconnect.php');
+
+/*function doLogin($username,$password)
+{
+  
+    $connect = new mysqlConnect('127.0.0.1','ccagUser','12345','ccagDB');
+
+    return $connect->loginAccount($username, $password);
+    
+}
+
+function doRegistration($username,$password,$email)
+{
+  $connect = new mysqlConnect('127.0.0.1','ccagUser','12345','ccagDB');
+
+  return $connect->registerAccount($username, $email, $password);
+}
+function doValidate($token){
+
+  $connect = new mysqlConnect('127.0.0.1','ccagUser','12345','ccagDB');
+
+  return $connect->validateSession($token);
+  
+}
+
+function doLogout($token) {
+  $connect = new mysqlConnect('127.0.0.1','ccagUser','12345','ccagDB');
+
+  return $connect->invalidateSession($token);
+}*/
+
+function getRecipe($info)
+{
+  //DMZ stuff here
+  return true;
+}
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "getRecipe":
+      return getRecipe($request);
+    default:
+      return "type fail".PHP_EOL;
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","DMZServer");
+
+echo "DMZServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "DMZServer END".PHP_EOL;
+exit();
+?>
+
+
