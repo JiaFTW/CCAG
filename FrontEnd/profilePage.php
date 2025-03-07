85800536ea0658d0812e05a1c7f8757b3973eccb
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-03-07 15:02:05 -0500

    Create profilePage.php

---
+++ FrontEnd/profilePage.php
@@ -0,0 +1,18 @@
+<?php
+require_once('sessionValidate.php')
+?>
+
+
+<html>
+    <head>
+    <title>CCAG Profile</title>
+    <link rel="stylesheet" href="./styles/styles.css">
+  </head>
+    <body>
+    <?php include('header.php'); ?>
+    <?php include('headerprofile.php'); ?>
+        <main>
+            <p></p>
+        </main>
+    </body>
+</html>
