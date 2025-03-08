17f391127e9f65141ea11a6fa471ff1923a89440
Gary Shi <153547689+JiaFTW@users.noreply.github.com>
2025-03-07 22:09:35 -0500

    Update search.php

--- FrontEnd/search.php
+++ FrontEnd/search.php
@@ -1,14 +1,34 @@
 <?php 
 require_once('../rabbitmq/testRabbitMQClient.php');
 
-$recipedata = array (
+/*$recipedata = array (
     'type' => 'getRecipe',
     'username' => $_COOKIE['username'],
     'keyword' => filter_input(INPUT_POST,'search'),
 );
 
 //Sends the login request
-$response = sendMessage($recipedata);
+$response = sendMessage($recipedata);*/
 
+//Test Code (To Do: Replace with real API call)
+$mockResponse = [
+    [
+        "name" => "Creamy Garlic Pasta",
+        "image" => "https://via.placeholder.com/250",
+        "url" => "https://example.com/recipe1",
+    ],
+    [
+        "name" => "Spaghetti Carbonara",
+        "image" => "https://via.placeholder.com/250",
+        "url" => "https://example.com/recipe2",
+    ],
+    [
+        "name" => "Pesto Pasta",
+        "image" => "https://via.placeholder.com/250",
+        "url" => "https://example.com/recipe3",
+    ]
+];
+
+echo json_encode($mockResponse); 
 
 ?>
