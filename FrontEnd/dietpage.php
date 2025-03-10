c9a24b0da239023c1f4c54fb2b99b0fab74c6a69
JiaFTW <shigary99@gmail.com>
2025-03-10 00:53:32 +0000

    Im crashing out

--- FrontEnd/dietpage.php
+++ FrontEnd/dietpage.php
@@ -33,66 +33,66 @@ function isChecked($match, $selectedRestrictions) {
                     <h2><label>Allergens</label></h2> <br>
 
                     <label for="dairy-free">Dairy-Free</label>
-                    <input type="checkbox" name="dairy-free" id="dairy-free" <?php isChecked('dairy-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="dairy-free" id="dairy-free" <?php echo isChecked('dairy-free', $restrictions); ?>> <br>
 
                     <label for="egg-free">Egg-Free</label>
-                    <input type="checkbox" name="egg-free" id="egg-free" <?php isChecked('egg-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="egg-free" id="egg-free" <?php echo isChecked('egg-free', $restrictions); ?>> <br>
 
                     <label for="peanut-free">Peanut-Free</label>
-                    <input type="checkbox" name="peanut-free" id="peanut-free"<?php isChecked('peanut-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="peanut-free" id="peanut-free"<?php echo isChecked('peanut-free', $restrictions); ?>> <br>
 
                     <label for="tree-nut-free">Tree-Nut-Free</label>
-                    <input type="checkbox" name="tree-nut-free" id="tree-nut-free"<?php isChecked('tree-nut-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="tree-nut-free" id="tree-nut-free"<?php echo isChecked('tree-nut-free', $restrictions); ?>> <br>
 
                     <label for="wheat-free">Wheat-Free</label>
-                    <input type="checkbox" name="wheat-free" id="wheat-free"<?php isChecked('wheat-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="wheat-free" id="wheat-free"<?php echo isChecked('wheat-free', $restrictions); ?>> <br>
 
                     <label for="soy-free">Soy-Free</label>
-                    <input type="checkbox" name="soy-free" id="soy-free"<?php isChecked('soy-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="soy-free" id="soy-free"<?php echo isChecked('soy-free', $restrictions); ?>> <br>
 
                     <label for="fish-free">Fish-Free</label>
-                    <input type="checkbox" name="fish-free" id="fish-free"<?php isChecked('fish-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="fish-free" id="fish-free"<?php echo isChecked('fish-free', $restrictions); ?>> <br>
 
                     <label for="shellfish-free">Shellfish-Free</label>
-                    <input type="checkbox" name="shellfish-free" id="shellfish-free"<?php isChecked('shellfish-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="shellfish-free" id="shellfish-free"<?php echo isChecked('shellfish-free', $restrictions); ?>> <br>
 
                     <label for="sesame-free">Sesame-Free</label>
-                    <input type="checkbox" name="sesame-free" id="sesame-free"<?php isChecked('sesame-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="sesame-free" id="sesame-free"<?php echo isChecked('sesame-free', $restrictions); ?>> <br>
 
                     <h2><label>Diet Restrictions</label></h2> <br>
 
                     <label for="gluten-free">Gluten-Free</label>
-                    <input type="checkbox" name="gluten-free" id="gluten-free"<?php isChecked('gluten-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="gluten-free" id="gluten-free"<?php echo isChecked('gluten-free', $restrictions); ?>> <br>
 
                     <label for="alcohol-free">Alcohol-Free</label>
-                    <input type="checkbox" name="alcohol-free" id="alcohol-free"<?php isChecked('alcohol-free', $restrictions) ?>> <br>
+                    <input type="checkbox" name="alcohol-free" id="alcohol-free"<?php echo isChecked('alcohol-free', $restrictions); ?>> <br>
 
                     <label for="kosher">Kosher</label>
-                    <input type="checkbox" name="kosher" id="kosher"<?php isChecked('kosher', $restrictions) ?>> <br>
+                    <input type="checkbox" name="kosher" id="kosher"<?php echo isChecked('kosher', $restrictions); ?>> <br>
 
                     <label for="keto-friendly">Keto</label>
-                    <input type="checkbox" name="keto-friendly" id="keto-friendly"<?php isChecked('keto-friendly', $restrictions) ?>> <br>
+                    <input type="checkbox" name="keto-friendly" id="keto-friendly"<?php echo isChecked('keto-friendly', $restrictions); ?>> <br>
 
                     <label for="vegetarian">Vegetarian</label>
-                    <input type="checkbox" name="vegetarian" id="vegetarian"<?php isChecked('vegetarian', $restrictions) ?>> <br>
+                    <input type="checkbox" name="vegetarian" id="vegetarian"<?php echo isChecked('vegetarian', $restrictions); ?>> <br>
 
                     <label for="high-fiber">High-Fiber</label>
-                    <input type="checkbox" name="high-fiber" id="high-fiber"<?php isChecked('high-fiber', $restrictions) ?>> <br>
+                    <input type="checkbox" name="high-fiber" id="high-fiber"<?php echo isChecked('high-fiber', $restrictions); ?>> <br>
 
-                    <label for="high-protein">High-Protein</label>
-                    <input type="checkbox" name="high-protein" id="high-protein"<?php isChecked('high-protein', $restrictions) ?>> <br>
+                    <label for="high-protein">FUCK High-Protein</label>
+                    <input type="checkbox" name="high-protein" id="high-protein" <?php echo isChecked('high-protein', $restrictions); ?>> <br>
 
                     <label for="low-carb">Low-Carb</label>
-                    <input type="checkbox" name="low-carb" id="low-carb"<?php isChecked('low-carb', $restrictions) ?>> <br>
+                    <input type="checkbox" name="low-carb" id="low-carb"<?php echo isChecked('low-carb', $restrictions); ?>> <br>
 
                     <label for="low-fat">Low-Fat</label>
-                    <input type="checkbox" name="low-fat" id="low-fat"<?php isChecked('low-fat', $restrictions) ?>> <br>
+                    <input type="checkbox" name="low-fat" id="low-fat"<?php echo isChecked('low-fat', $restrictions); ?>> <br>
 
                     <label for="low-sodium">Low-Sodium</label>
-                    <input type="checkbox" name="low-sodium" id="low-sodium"<?php isChecked('low-sodium', $restrictions) ?>> <br>
+                    <input type="checkbox" name="low-sodium" id="low-sodium"<?php echo isChecked('low-sodium', $restrictions); ?>> <br>
 
                     <label for="low-sugar">Low-Sugar</label>
-                    <input type="checkbox" name="low-sugar" id="low-sugar"<?php isChecked('low-sugar', $restrictions) ?>> <br>
+                    <input type="checkbox" name="low-sugar" id="low-sugar"<?php echo isChecked('low-sugar', $restrictions); ?>> <br>
 
                     <button type="submit" class="save-button">Save</button>
                 </form>
@@ -113,7 +113,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         'alcohol-free', 'kosher', 'keto-friendly'
     ];
 
-    foreach ($allRestrictions as $key => $label) {
+        echo '<pre>';
+        print_r($_POST);
+        echo '</pre>';
+
+
+    foreach ($restriction_keys as $key) {
         if (isset($_POST[$key])) {
             $restrictions[] = $key;
         }
@@ -125,8 +130,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         'restrictions' => $restrictions
     );
 
+
+    echo '<pre>';
+    print_r($dietdata);
+    echo '</pre>';
+
     $response = sendMessage($dietdata);
 
- 
+    header("Location: homepage.php");
+    die();
 }
 ?>
