c046e5be9ece792c8f63ab30421376fd60e632fa
Al Kazi <ama275@njit.edu>
2025-03-11 12:37:24 -0400

    done

--- rabbitmq/DMZServer.php
+++ rabbitmq/DMZServer.php
@@ -4,31 +4,67 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 require_once('../Database/mysqlconnect.php');
+require_once('../DMZ/edamamProcessor.php');
 
-function getRecipe($info)
+
+function searchRecipe($query)
 {
-  //DMZ stuff here
-  echo "getting here".PHP_EOL;
-  return true;
+	// getting data from Edamam API
+	$recipes = fetchEdamamData($query);
+
+	// checks if recipes were fetched successfully
+	if (!empty($recipes))
+	{
+		return [
+			'status' => 'success',
+			'message' => 'Recipes fetched successfully',
+            		'recipes' => $recipes, // returns the filtered recipes
+	       	];
+	}
+	else
+       	{
+	       	return [
+            	'status' => 'error',
+            	'message' => 'Failed to fetch recipes from Edamam API',
+        	];
+    	}
 }
 
 function requestProcessor($request)
 {
-  echo "received request".PHP_EOL;
-  var_dump($request);
-  if(!isset($request['type']))
-  {
-    return "ERROR: unsupported message type";
-  }
-  switch ($request['type'])
-  {
-    case "getRecipe":
-      return getRecipe($request);
-    default:
-      return "type fail".PHP_EOL;
-  }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+	echo "received request".PHP_EOL;
+  	var_dump($request);
+
+  	if (!isset($request['type'])) 
+       	{
+	       	return [
+		       	'status' => 'error',
+		  	'message' => 'Unsupported message type',
+		];
+	}
+
+	switch ($request['type']) 
+	{
+	case 'searchRecipe':
+
+		// handle fetching recipes from Edamam API
+		if (!isset($request['query'])) 
+		{
+                	return [
+			       	'status' => 'error',
+			       	'message' => 'Query parameter is missing',
+			];
+		}
+		return searchRecipe($request['query']);
+
+	default:
+		return [
+			'status' => 'error',
+			'message' => 'Unsupported request type',
+		];
+	}
 }
+
 
 $server = new rabbitMQServer("testRabbitMQ.ini","DMZServer");
 
@@ -37,5 +73,6 @@ $server->process_requests('requestProcessor');
 echo "DMZServer END".PHP_EOL;
 exit();
 ?>
+
 
 
