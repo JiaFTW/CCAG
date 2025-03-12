ce7d942893d85997acc793fd8f1ab572849c3ff0
Chris <cn7@njit.edu>
2025-03-12 19:38:47 +0000

    Removed database include from DMZ server

--- rabbitmq/DMZServer.php
+++ rabbitmq/DMZServer.php
@@ -3,7 +3,7 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('../Database/mysqlconnect.php');
+//require_once('../Database/mysqlconnect.php');
 require_once('../DMZ/edamamProcessor.php');
 
 
