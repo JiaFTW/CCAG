610c1f0b524d895175122c0a0975998be9df32a6
JiaFTW <shigary99@gmail.com>
2025-03-12 01:12:17 +0000

    Currently adding skeleton array for MP

---
+++ FrontEnd/addMealPlan.php
@@ -0,0 +1,51 @@
+<?php
+require_once('../rabbitmq/testRabbitMQClient.php');
+
+$addMealPlan = array (
+    'type' => ,
+    'username' => ,
+    'rid' => ,
+
+
+    //Monday
+    'MON1' => ,
+    'MON2' => ,
+    'MON3' => ,
+
+    //Tuesday
+    'TUE1' => ,
+    'TUE2' => ,
+    'TUE3' => ,
+
+    //Wednesday
+    'WED1' => ,
+    'WED2' => ,
+    'WED3' => ,
+
+    //Thursday
+    'THU1' => ,
+    'THU2' => ,
+    'THU3' => ,
+
+    //Friday
+    'FRI1' => ,
+    'FRI2' => ,
+    'FRI3' => ,
+
+    //Saturday
+    'SAT1' => ,
+    'SAT2' => ,
+    'SAT3' => ,
+
+    //Sunday
+    'SUN1' => ,
+    'SUN2' => ,
+    'SUN3' => ,
+);
+
+
+$response = sendMessage($addMealPlan);
+
+header("Location: mppage.php")
+die();
+?>
