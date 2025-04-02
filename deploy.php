8631e7e7f3bdb56ba6717898e5d87df73c1be47c
Chris <cn7@njit.edu>
2025-04-02 17:25:58 +0000

    Beginning of deploy file

---
+++ deploy.php
@@ -0,0 +1,120 @@
+#! /usr/bin/php
+<?php
+    $toSend = array();
+    $toSend["db"] = false;
+    $toSend["dmz"] = false;
+    $toSend["front"] = false;
+    $toSend["rabbit"] = false;
+    $opener = "Welcome to the CCAG deployment system!
+    \nPlease enter what folder(s) you wish to deploy, then type \"0\" when done.
+    \nCodes are as follows:
+    \nDatabase - 1
+    \nDMZ\t - 2
+    \nFrontEnd - 3
+    \nRabbitMQ - 4
+    \nAll\t - 5
+    \nCancel\t - 6" . PHP_EOL;
+    echo $opener;
+
+    $entry = readline("Please enter a code: ");
+
+    while($entry != 0)
+    {
+        if(!is_numeric($entry))
+        {
+            echo "Only numbers from 0-6 are valid codes" . PHP_EOL;
+            $entry = readline("Please enter a code: ");
+            continue;
+        }
+
+        switch($entry)
+        {
+            case 1:
+                $toSend["db"] = true;
+                break;
+            case 2:
+                $toSend["dmz"] = true;
+                break;
+            case 3:
+                $toSend["front"] = true;
+                break;
+            case 4:
+                $toSend["rabbit"] = true;
+                break;
+            case 5:
+                $toSend["db"] = true;
+                $toSend["dmz"] = true;
+                $toSend["front"] = true;
+                $toSend["rabbit"] = true;
+                break;
+            case 6:
+                echo "Cancelling deployment!" . PHP_EOL;
+                exit();
+            default:
+                echo "Invalid code number" . PHP_EOL;
+        }
+
+        if($entry == 5)
+        {
+            echo "All selected!" . PHP_EOL;
+            break;
+        }
+
+        $entry = readline("Please enter a code: ");
+    }
+
+    if(!$toSend["db"] && !$toSend["dmz"] && !$toSend["front"] && !$toSend["rabbit"])
+    {
+        echo "Nothing selected! Closing file..." . PHP_EOL;
+        exit();
+    }
+    
+    $locText = "Please select where these files will be deployed:
+    \nQA\t   - 1
+    \nProduction - 2
+    \nCancel\t   - 3" .PHP_EOL;
+    echo $locText;
+
+    $entry = readline("Please enter a code: ");
+    $ready = false;
+    $location;
+
+    while(!$ready)
+    {
+        if(!is_numeric($entry))
+        {
+            echo "Only numbers from 1-3 are valid codes" . PHP_EOL;
+            $entry = readline("Please enter a code: ");
+            continue;
+        }
+        
+        switch($entry)
+        {
+            case 1:
+                $location = "QA";
+                $ready = true;
+                break;
+            case 2:
+                $location = "Pr0duction";
+                $ready = true;
+                break;
+            case 3:
+                echo "Cancelling deployment!" . PHP_EOL;
+                exit();
+            default:
+                echo "Invalid code number" . PHP_EOL;
+        }
+
+        if(!$ready)
+        $entry = readline("Please enter a code: ");
+    }
+
+    $id = rand();
+    echo $id .PHP_EOL;
+    $filename;
+
+    /*if($toSend["db"])
+    {
+        $filename = ""
+    }*/
+?>
