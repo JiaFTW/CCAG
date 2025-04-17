2d62b8cd32a408075d90bc9003db58c6c5d062ab
JiaFTW <shigary99@gmail.com>
2025-04-17 16:34:08 +0000

    yaes

--- FrontEnd/testpage.php
+++ FrontEnd/testpage.php
@@ -1,4 +1,20 @@
 <?php
 require_once('./logging/writelog.php');
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
 writelog("SAYS HI", "BOB"); 
+}
+
 ?>
+
+
+<!DOCTYPE html>
+<html>
+  <body>
+    <h2>Testing</h2>
+    <form method="POST"> 
+      <button type="submit">BOB</button>
+    </form>
+  </body>
+</html>
